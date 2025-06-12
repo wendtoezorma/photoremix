71c7b8eccad4c0b514ef082d6cb36f291e89811f
wendtoezorma <wendtoezorma@gmail.com>
2025-06-12 10:33:21 +0200

    mise a jour css

--- uploader/views.py
+++ uploader/views.py
@@ -19,21 +19,21 @@ def index(request):
         width, height = user_img.size
         crop_height = int(height * 2.5 / 3)
         user_img = user_img.crop((0, 0, width, crop_height))
-
+        size = 420
         # Redimensionne en carré (pour cercle parfait)
-        user_img = user_img.resize((400, 400))
+        user_img = user_img.resize((size, size))
 
         # Crée un masque circulaire
-        mask = Image.new('L', (400, 400), 0)
+        mask = Image.new('L', (size, size), 0)
         draw = ImageDraw.Draw(mask)
-        draw.ellipse((0, 0, 400, 400), fill=255)
+        draw.ellipse((0, 0, size, size), fill=255)
 
         # Applique le masque à l'image et créer un fond transparent
-        circular_img = Image.new('RGBA', (400, 400), (0, 0, 0, 0))
+        circular_img = Image.new('RGBA', (size, size), (0, 0, 0, 0))
         circular_img.paste(user_img, (0, 0), mask)
         
         # Position où coller sur la base
-        position = (0, 625)
+        position = (-35, 630)
 
         # Coller l'image circulaire
         base_img.paste(circular_img, position, circular_img)
@@ -56,3 +56,52 @@ def index(request):
         return render(request, 'index.html', {'photo': photo_obj})
 
     return render(request, 'index.html')
+
+
+
+'''
+def index(request): 
+    if request.method == 'POST' and request.FILES.get('photo'):
+        uploaded_photo = request.FILES['photo']
+
+        base_path = 'originals/base.jpg'
+        base_img = Image.open(f'media/{base_path}').convert('RGBA')
+        user_img = Image.open(uploaded_photo).convert('RGBA')
+
+        width, height = user_img.size
+        crop_height = int(height * 2.5 / 3)
+        user_img = user_img.crop((0, 0, width, crop_height))
+
+        # Nouvelle taille carrée plus petite
+        size = int(350)
+
+        user_img = user_img.resize((size, size))
+
+        mask = Image.new('L', (size, size), 255)
+
+        # Image avec fond transparent
+        square_img = Image.new('RGBA', (size, size), (0, 0, 0, 0))
+        square_img.paste(user_img, (0, 0), mask)
+
+        # Ajuste la position selon la nouvelle taille (exemple)
+        position = (20, 675)  # Tu peux ajuster X ou Y ici si besoin
+
+        base_img.paste(square_img, position, square_img)
+
+        result = base_img.convert('RGB')
+
+        buffer = io.BytesIO()
+        result.save(buffer, format='JPEG')
+        image_file = ContentFile(buffer.getvalue(), name='result.jpg')
+
+        photo_obj = UploadedPhoto.objects.create(
+            original=uploaded_photo,
+            base=base_path,
+            result=image_file
+        )
+
+        return render(request, 'index.html', {'photo': photo_obj})
+
+    return render(request, 'index.html')
+
+'''
